afedda6355895cb26b9355440235934dfda9c107
HectorFelix <mac@Hectors-Mac-Pro.local>
2021-04-29 11:43:49 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1a85e64da0f70b467ca2.js"
+    "url": "webpack-runtime-b1493ab925db6200011f.js"
   },
   {
     "url": "framework-305b3707783ccc9d7ca6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "077c0ab53d108f9e3a728183fd25e4d6"
+    "revision": "c44ca5de75b5c63356e864f97833c9cb"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
